63d6027eadf0dcb9ec5e134007844731d37d7083
alazzuri <alexislazzuri@gmail.com>
2021-07-01 19:38:38 -0300

    Minor fixing

--- src/config/typeorm.js
+++ src/config/typeorm.js
@@ -15,7 +15,7 @@ export async function connect() {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     entities: [UserEntity, TaskEntity],
-    synchronize: !!IS_PRODUCTION,
+    synchronize: !IS_PRODUCTION,
     ssl: { rejectUnauthorized: false },
   });
 
